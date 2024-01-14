1075632b19b4483c2d28ab48049dc2eb805ccd09
Ichi Pravda <ichipravda@IchiPravda.IchiPravda.com>
2024-01-14 04:51:59 +0100

    small changes

--- js/chart.js
+++ js/chart.js
@@ -16,7 +16,7 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(".beard-total").textContent = porto[1] + " x 20PLN";
     document.querySelector(".total").textContent = ((portfolio1Value * 50) + (portfolio2Value * 20)) + " PLN";
 
-    emailbody = "Your Order is :\n" + "Haircut : " + portfolio1Value + " x 50 PLN" + "\nBeard Shaving : " + portfolio2Value + " x 20 PLN\n";
+    emailbody = "Your Order is :\n" + "Haircut : " + portfolio1Value + " x 50 PLN" + "\nBeard Shaving : " + portfolio2Value + " x 20 PLN\n" + "\nThe Total is : " + ((portfolio1Value * 50) + (portfolio2Value * 20)) + " PLN";
 });
 
 function backBtn() {
@@ -27,8 +27,7 @@ function reservation() {
     let name = document.getElementById('fullname').value;
     let selectedDate = document.getElementById('datepick').value;
     let dateString = String(selectedDate);
-    let msg = name + "\n" + emailbody + "\nWith Date : " + dateString;
-    let num = "2348178277291";
+    let msg = name + "\n " + emailbody + "\nWith Date : " + dateString;
     let url = "https://wa.me/2348178277291"
 
     let end_url = `${url}?text=${msg}`;
